6c747e5372192ec64b4013f3a6a4765aed9c4892
Blake Rose <bdecktor@gmail.com>
2019-07-28 18:00:50 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -50,12 +50,3 @@ def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_b
     WHERE category = 'books';"
 end
 
-
-def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-end
-
-def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-end
-
-def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-end
